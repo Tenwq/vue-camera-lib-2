57db85067812139c05a2f0b8a98e8efb4dd70b85
wangqi <494313342@qq.com>
2023-09-12 18:26:46 +0800

    bugfix

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 import WebCam from "./src/Webcam.vue";
 import WebCamUI from "./src/WebCamUI.vue";
 export default {
-    install: (app) => {
-        app.component("WebCam", WebCam);
-        app.component("WebCamUI", WebCamUI);
-        
-    },
+  install: (app) => {
+    app.component("WebCam", WebCam);
+    app.component("WebCamUI", WebCamUI);
+  },
 }
 
-export { WebCam , WebCamUI };
+export { WebCam, WebCamUI };
